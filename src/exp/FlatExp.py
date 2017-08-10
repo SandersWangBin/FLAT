bf688548575bd7aacdbc528f9cff2ca346e81aac
SandersWangBin <sanderswangbin@gmail.com>
2017-08-10 14:58:32 +0200

    FLAT: move data to FlatExpCons
    Move all of configure data to FlatExpCons.

---
+++ src/exp/FlatExp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+from FlatExpCons import * 
+
+import sys
+sys.path.append('../')
+from lib.FlatInt import FlatInt
+from lib.FlatStr import FlatStr
+from lib.FlatList import FlatList
+
+class FlatExp:
+    @staticmethod
+    def parser(ruleExp):
+        ruleExpDoc = eval(ruleExp)
+        if ruleExpDoc[EXP_TYPE] in EXP_TYPE_VALUES:
+            return FlatExp.generateObj(ruleExpDoc, ruleExpDoc[EXP_TYPE])
+
+    @staticmethod
+    def generateObj(ruleExpDoc, ruleExpType):
+        argTuple = tuple()
+        for argName in EXP_RULE_VALUES[ruleExpType]:
+            argValue = ruleExpDoc[EXP_RULE].get(argName, None)
+            argTuple = argTuple + (argValue,)
+        obj = eval(EXP_CLASS_VALUES[ruleExpType])
+        return obj
---
+++ src/exp/FlatExpCons.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+EXP_TYPE = 'type'
+EXP_TYPE_INT = 'int'
+EXP_TYPE_STR = 'str'
+EXP_TYPE_BOOL = 'bool'
+EXP_TYPE_ENUM = 'enum'
+EXP_TYPE_LIST = 'list'
+
+EXP_RULE = 'rule'
+EXP_RULE_RANGE = 'range'
+EXP_RULE_DEFAULT = 'default'
+EXP_RULE_REGEX = 'regex'
+EXP_RULE_LENGTH = 'length'
+EXP_RULE_ELEMENT = 'element'
+
+EXP_TYPE_VALUES = [EXP_TYPE_INT, \
+                   EXP_TYPE_STR, \
+                   EXP_TYPE_BOOL, \
+                   EXP_TYPE_ENUM, \
+                   EXP_TYPE_LIST]
+
+EXP_RULE_VALUES = {EXP_TYPE_INT: \
+                       [EXP_RULE_RANGE], \
+                    EXP_TYPE_STR: \
+                       [EXP_RULE_DEFAULT, \
+                        EXP_RULE_REGEX, \
+                        EXP_RULE_LENGTH], \
+                    EXP_TYPE_BOOL: [], \
+                    EXP_TYPE_ENUM: \
+                        [EXP_RULE_RANGE],
+                    EXP_TYPE_LIST: \
+                        [EXP_RULE_ELEMENT]}
+
+EXP_CLASS_VALUES = {EXP_TYPE_INT:  'FlatInt(argTuple)',
+                    EXP_TYPE_STR:  'FlatStr(argTuple)',
+                    EXP_TYPE_BOOL: 'FlatBool(argTuple)',
+                    EXP_TYPE_ENUM: 'FlatEnum(argTuple)',
+                    EXP_TYPE_LIST: 'FlatList(argTuple)'}
