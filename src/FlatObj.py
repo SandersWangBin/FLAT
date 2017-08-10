c41c1a33a13b81ad0447ff141dd62b884f4a7576
SandersWangBin <sanderswangbin@gmail.com>
2017-08-10 14:57:08 +0200

    FLAT: rename FlatExpRule to FlatExp
    Rename FlatExpRule to FlatExp, because FlatExp can handle both RuleExp and outExp.

--- src/FlatObj.py
+++ src/FlatObj.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 import re
-from exp.FlatRuleExp import *
+from exp.FlatExp import *
 
 class FlatObj:
     REG_FLAT_OBJ_EXP = r"j'(.*)'.FLAT\((.*)\)"
 
     def __init__(self, flatExp, objName=None):
         self.objName = objName
-        self.ruleExp, self.outExp = self.initFlatObj(flatExp)
-        self.ruleTuple = FlatRuleExp.parser(self.ruleExp)
-        print self.ruleTuple
+        ruleExp, outExp = self.initFlatObj(flatExp)
+        self.ruleObj = FlatExp.parser(ruleExp)
+        self.outObj = FlatExp.parser(outExp)
 
     def initFlatObj(self, flatExp):
         match = re.search(self.REG_FLAT_OBJ_EXP, flatExp)
@@ -22,7 +22,7 @@ class FlatObj:
 
     def __str__(self):
         returnStr = "==== FlatObj Debug Info ====\n" + \
-            "= objName : " + str(self.objName) + "\n" + \
-            "= ruleExp : " + str(self.ruleExp) + "\n" + \
-            "= outExp  : " + str(self.outExp) + "\n"
+            "= objName: " + str(self.objName) + "\n" + \
+            "= ruleObj: " + str(self.ruleObj) + "\n" + \
+            "= outObj : " + str(self.outObj) + "\n"
         return returnStr
