d6d3c85b1f782530cf9711c229d8d0bc6d6d2011
SandersWangBin <sanderswangbin@gmail.com>
2017-08-10 14:54:41 +0200

    FLAT: update the testcase
    Update the testcase as:
    - Update the outExp format to align with ruleExp
    - print the FlatObj to manual track

--- test/test.py
+++ test/test.py
@@ -4,8 +4,11 @@ import sys
 sys.path.append('../src/')
 from FlatObj import FlatObj
 
-flatExp01 = """j'{"type":"int", "rule":{"range":[0,2,4,6]}}'.FLAT({"out":"list", "format":"value if matched else None"})"""
-flatExp02 = """j'{"type":"str", "rule":{"regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"out":"list", "format":"(value,'POS' if matched else 'NEG')"})"""
+flatExp01 = """j'{"type":"int", "rule":{"range":[0,2,4,6]}}'.FLAT({"type":"list", "rule":{"element":"value if matched else None"}})"""
+flatExp02 = """j'{"type":"str", "rule":{"regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"type":"list", "rule":{"element":"(value,'POS' if matched else 'NEG')"}})"""
 
 flatObj01 = FlatObj(flatExp01)
+print flatObj01
+
 flatObj02 = FlatObj(flatExp02)
+print flatObj02
