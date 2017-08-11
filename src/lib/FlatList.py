4a36d34dc313fe252154af433b476fe6b2dc8af8
SandersWangBin <sanderswangbin@gmail.com>
2017-08-11 13:08:30 +0200

    FLAT: add generatedElement in FlatList

--- src/lib/FlatList.py
+++ src/lib/FlatList.py
@@ -3,8 +3,9 @@
 class FlatList:
     def __init__(self, arg):
         self.initElement, = arg
+        self.generatedElement = self.initElement
 
     def __str__(self):
         return '==== FlatList debug info ====\n' + \
-               '= original element: ' + str(self.initElement) + '\n'
-
+               '= original element : ' + str(self.initElement) + '\n' + \
+               '= generated element: ' + str(self.generatedElement) + '\n'
