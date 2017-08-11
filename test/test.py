6c379ab449f12cddb5aeed43d662bef793d2b7ac
SandersWangBin <sanderswangbin@gmail.com>
2017-08-11 10:33:56 +0200

    FLAT: test cases updated

--- test/test.py
+++ test/test.py
@@ -5,10 +5,14 @@ sys.path.append('../src/')
 from FlatObj import FlatObj
 
 flatExp01 = """j'{"type":"int", "rule":{"range":[0,2,4,6]}}'.FLAT({"type":"list", "rule":{"element":"value if matched else None"}})"""
-flatExp02 = """j'{"type":"str", "rule":{"regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"type":"list", "rule":{"element":"(value,'POS' if matched else 'NEG')"}})"""
+flatExp02 = """j'{"type":"int", "rule":{"range":"[12, 16] + range(0,10)"}}'.FLAT({"type":"list", "rule":{"element":"value if !matched else None"}})"""
+flatExp03 = """j'{"type":"str", "rule":{"regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"type":"list", "rule":{"element":"(value,'POS' if matched else 'NEG')"}})"""
 
 flatObj01 = FlatObj(flatExp01)
 print flatObj01
 
 flatObj02 = FlatObj(flatExp02)
 print flatObj02
+
+flatObj03 = FlatObj(flatExp03)
+print flatObj03
