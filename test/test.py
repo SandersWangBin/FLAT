477666bb0d29a3df9d32b39fadc8a501a22a126c
SandersWangBin <sanderswangbin@gmail.com>
2017-08-11 13:35:27 +0200

    FLAT: add verification in the test case

--- test/test.py
+++ test/test.py
@@ -7,13 +7,20 @@ from FlatObj import FlatObj
 flatExp01 = """j'{"type":"int", "rule":{"range":[0,2,4,6]}}'.FLAT({"type":"list", "rule":{"element":"value if matched else None"}})"""
 flatExp02 = """j'{"type":"int", "rule":{"range":"[12, 16] + range(0,10)"}}'.FLAT({"type":"list", "rule":{"element":"value if not matched else None"}})"""
 flatExp03 = """j'{"type":"int", "rule":{"range":range(0,3) + range(4,7)}}'.FLAT({"type":"list", "rule":{"element":"(value, matched, desc)"}})"""
-flatExp04 = """j'{"type":"str", "rule":{"regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"type":"list", "rule":{"element":"(value,'POS' if matched else 'NEG')"}})"""
+flatExp04 = """j'{"type":"str", "rule":{"default":"FlatTest01234", "regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"type":"list", "rule":{"element":"(value,'POS' if matched else 'NEG')"}})"""
+
+def verify(flatObj, expectedList):
+    print 'FLAT EXP: ', flatObj.flatExp
+    print 'FLAT OUT: ', str(flatObj.output)
+    print 'Expected: ', str(expectedList)
+    print ' => ', str(flatObj.output == expectedList)
+    print
 
 flatObj01 = FlatObj(flatExp01)
-print flatObj01
+verify(flatObj01, [0, 2, 4, 6])
 
 flatObj02 = FlatObj(flatExp02)
-print flatObj02
+verify(flatObj02, [-1, 10, 11, 13, 15, 17])
 
 flatObj03 = FlatObj(flatExp03)
-print flatObj03
+verify(flatObj03, [(-1, False, '__INT_-1'), (0, True, '__INT_0'), (2, True, '__INT_2'), (3, False, '__INT_3'), (4, True, '__INT_4'), (6, True, '__INT_6'), (7, False, '__INT_7')])
