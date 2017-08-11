4aa6ba38cd6613b6ab596d6742d39eb3119d954d
SandersWangBin <sanderswangbin@gmail.com>
2017-08-11 13:03:47 +0200

    FLAT: add new flat expression in test case

--- test/test.py
+++ test/test.py
@@ -5,8 +5,9 @@ sys.path.append('../src/')
 from FlatObj import FlatObj
 
 flatExp01 = """j'{"type":"int", "rule":{"range":[0,2,4,6]}}'.FLAT({"type":"list", "rule":{"element":"value if matched else None"}})"""
-flatExp02 = """j'{"type":"int", "rule":{"range":"[12, 16] + range(0,10)"}}'.FLAT({"type":"list", "rule":{"element":"value if !matched else None"}})"""
-flatExp03 = """j'{"type":"str", "rule":{"regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"type":"list", "rule":{"element":"(value,'POS' if matched else 'NEG')"}})"""
+flatExp02 = """j'{"type":"int", "rule":{"range":"[12, 16] + range(0,10)"}}'.FLAT({"type":"list", "rule":{"element":"value if not matched else None"}})"""
+flatExp03 = """j'{"type":"int", "rule":{"range":range(0,3) + range(4,7)}}'.FLAT({"type":"list", "rule":{"element":"(value, matched, desc)"}})"""
+flatExp04 = """j'{"type":"str", "rule":{"regex":"[a-zA-Z0-9_]", "length":"range(1,255)"}}'.FLAT({"type":"list", "rule":{"element":"(value,'POS' if matched else 'NEG')"}})"""
 
 flatObj01 = FlatObj(flatExp01)
 print flatObj01
